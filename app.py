c8b5f97f7f48b118cbf143b553798c6bac790844
Will Huang <s951553@gmail.com>
2018-10-23 17:57:27 +0800

    1. Add read/write drama information to file. 2. Provide main application to run it.

---
+++ app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from crawler.jp_drama_crawler import JPDramaCrawler
+from data.drama_info import DramaInfo
+import data.drama_info
+
+if __name__ == "__main__":
+    crawler = JPDramaCrawler()
+    crawler.retrieve_url()
+    dramas = crawler.get_dramas()
+
+    data.drama_info.print_pretty_drama_infos(dramas)
+    data.drama_info.write_to_file("Dramas.json")
--- data/drama_info.py
+++ data/drama_info.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
+from tinydb import TinyDB, Query
 
 class DramaInfo:
     def __init__(self):
@@ -13,5 +14,39 @@ class DramaInfo:
         :param text(str): Text to parse.
         """
         tokens = re.split("第[0-9]+集", text.strip())
-        self.name = tokens[0].strip()
-        self.num = int(re.sub(r".*[Ee][Pp]", "", tokens[1]))
+        if len(tokens) > 1:
+            self.name = tokens[0].strip()
+            self.num = int(re.sub(r".*[Ee][Pp]", "", tokens[1]))
+
+def print_pretty_drama_infos(infos):
+    for i in infos:
+        print("%s   EP %d   %s" % (i.name, i.num, i.url))
+
+def write_to_file(path, infos):
+    """
+    :param path(str): Path to save drama info to database.
+    :param infos(list): Drama info list to write to database.
+    """
+    db = TinyDB(path)
+    for i in infos:
+        db.insert({"name": i.name, "num": i.num, "url": i.url})
+    db.close()
+
+def read_from_file(path):
+    """
+
+    :param path(str): Path to load drama info from database.
+    :return: List of drama info.
+    """
+    infos = []
+
+    db = TinyDB(path)
+    for i in db.all():
+        drama_info = DramaInfo
+        drama_info.name = i.name
+        drama_info.num = int(i.num)
+        drama_info.url = i.url
+        infos.append(drama_info)
+    db.close()
+
+    return infos
